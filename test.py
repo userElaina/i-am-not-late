ef38b3e934cd110f6ab2e39766862b03ff5757eb
Elaina <userelaina@pm.me>
2023-09-09 15:24:16 +0100

    :white_check_mark: update `test.py`

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import time
 from iamnotlate import notLate
 
-t = time.mktime(time.strptime("080000 23", "%H%M%S %y"))
+t = time.mktime(time.strptime("080800 23", "%H%M%S %y"))
 tmie = notLate(t, 'H')
-print(time.ctime(t+480))
-print(tmie.ctime(t+480))
+print(time.ctime(t))
+print(tmie.ctime(t))
 # Sun Jan  1 08:08:00 2023
 # Sun Jan  1 07:68:00 2023
 
